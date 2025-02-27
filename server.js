0f29725267c712db2331422089bfc3a41805e470
TSB-0516 <149648532+TSB-0516@users.noreply.github.com>
2025-02-27 18:30:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-
+// Add these after your CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 
 const PORT = 3000;
